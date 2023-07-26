d52c238a03ef6e2276d0cdea1b621a126b884f56
awaelchli <aedu.waelchli@gmail.com>
2023-07-26 16:46:30 +0200

    bug report

--- examples/pytorch/bug_report/bug_report_model.py
+++ examples/pytorch/bug_report/bug_report_model.py
@@ -1,66 +1,68 @@
 import os
 
 import torch
+from torch import nn
+from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader, Dataset
-
-from lightning.pytorch import LightningModule, Trainer
 
 
 class RandomDataset(Dataset):
-    def __init__(self, size, length):
-        self.len = length
-        self.data = torch.randn(length, size)
-
-    def __getitem__(self, index):
-        return self.data[index]
+    def __init__(self, shape, num_samples) -> None:
+        super().__init__()
+        self.shape = shape
+        self.num_samples = num_samples
 
     def __len__(self):
-        return self.len
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        arr = torch.randn(self.shape) ** (5 / 4)
+        for x in torch.randn(30000):
+            arr *= x
+        return arr
 
 
-class BoringModel(LightningModule):
+class BoringModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer = torch.nn.Linear(32, 2)
+        self.layer = torch.nn.Conv2d(3, 1, 1)
 
     def forward(self, x):
         return self.layer(x)
 
-    def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
 
-    def validation_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("valid_loss", loss)
+def run(rank, world_size):
+    cuda = False
 
-    def test_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("test_loss", loss)
+    print("init rank", rank)
+    os.environ["LOCAL_RANK"] = str(rank)
+    os.environ["WORLD_SIZE"] = str(world_size)
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "1234"
+    torch.distributed.init_process_group(
+        backend=("nccl" if cuda else "gloo"),
+        init_method="env://",
+        rank=rank,
+        world_size=world_size,
+    )
+    print("init rank done", rank)
 
-    def configure_optimizers(self):
-        return torch.optim.SGD(self.layer.parameters(), lr=0.1)
-
-
-def run():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
+    train = RandomDataset((3, 2048, 2048), 10000)
+    train = DataLoader(train, batch_size=32, pin_memory=True, num_workers=2)
 
     model = BoringModel()
-    trainer = Trainer(
-        default_root_dir=os.getcwd(),
-        limit_train_batches=1,
-        limit_val_batches=1,
-        limit_test_batches=1,
-        num_sanity_val_steps=0,
-        max_epochs=1,
-        enable_model_summary=False,
-    )
-    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
+    ddp_model = DistributedDataParallel(model, device_ids=([rank] if cuda else None))
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+    for i, batch in enumerate(train):
+        print(i)
+        prediction = ddp_model(batch)
+        loss = torch.nn.functional.mse_loss(prediction, torch.ones_like(prediction))
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
 
 if __name__ == "__main__":
-    run()
+    world_size = 2
+    torch.multiprocessing.spawn(run, nprocs=world_size, args=(world_size,))
